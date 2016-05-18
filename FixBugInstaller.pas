8a42e135b615c3a7c749300b2e4324ce357b6779
Motoarhangel <motoarhangel@bk.ru>
2016-05-18 15:09:53 +0600

    Add files via upload

---
+++ FixBugInstaller.pas
@@ -0,0 +1,37 @@
+// Fix bugs RAD installer from Russian language
+// by MotoArhangel ;)
+unit FixBugInstaller;
+
+interface
+uses
+  registry, SysUtils, Windows;
+
+  procedure Installlanguage(status:integer);
+
+implementation
+
+procedure Installlanguage(status:integer);
+var
+  Reg: TRegistry;
+begin
+  Reg := TRegistry.Create;
+  try
+    begin
+      Reg.RootKey := HKEY_LOCAL_MACHINE;
+      if Reg.openkey('SYSTEM\CurrentControlSet\Control\Nls\Language',false) then
+        case status of
+          //0419 - Russian language
+          //0409 - English language
+          0: if strtoint(reg.ReadString('InstallLanguage'))=0419
+               then reg.WriteString('InstallLanguage','0409');
+
+          1: if strtoint(reg.ReadString('InstallLanguage'))=0409
+               then reg.WriteString('InstallLanguage','0419');
+        end;
+    end;
+  finally
+    Reg.Free;
+  end;
+end;
+
+end.
